7a05e5447296349bcb8bb6d395f0aa233779e6f7
Meghana-Musale <musalemeghana3@gmail.com>
2025-04-29 22:09:57 -0700

    Add files via upload

---
+++ run time 1D array.c
@@ -0,0 +1,17 @@
+//1D array run time
+#include<stdio.h>
+int main()
+{
+	int i,a[5];
+	printf("Enter the elements of an array :");
+	for(i=0;i<5;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	printf("\n The Elements of array :");
+	for(i=0;i<5;i++)
+	{
+		printf("%d\n",a[i]);
+	}
+	
+}
