7316845032d96f7255e54f25a705cb6225db44c3
Meghana-Musale <musalemeghana3@gmail.com>
2025-05-05 18:28:23 +0530

    Add files via upload

---
+++ strlen.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char st[20];
+	int i;
+	printf("Enter string :");
+	gets(st);
+	i=strlen(st);
+	printf("String length : %d",i );
+
+return 0;
+}
