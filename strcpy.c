d9ae6bcf485f2a3699f061aa9b0286fae69c83fb
Meghana-Musale <musalemeghana3@gmail.com>
2025-05-08 19:56:12 +0530

    Add files via upload

---
+++ strcpy.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char st[20],nw[20];
+	printf("Enter 1st string");
+	gets(st);
+	printf("Enter 2nd string");
+	gets(nw);
+	strcpy(st,nw);
+	printf("string after copy :%s",st);
+	return 0;
+}
